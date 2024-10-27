452349861a70249feaa7233e4d757a1eccfe03d0
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2024-10-27 21:09:05 +0100

    add output tool test

--- tests/cli.rs
+++ tests/cli.rs
@@ -5,3 +5,10 @@ fn runs() {
     let mut cmd = Command::cargo_bin("panlabel").unwrap();
     cmd.assert().success();
 }
+
+#[test]
+fn outputs_tool_name() {
+    let mut cmd = Command::cargo_bin("panlabel").unwrap();
+    cmd.arg("-V");
+    cmd.assert().success().stdout("panlabel 0.1.0\n");
+}
